2f7862de8a7de1d42eda8d8cc55886d070f10afc
Ionut Cristian Mihoci <ionutmihoci@yahoo.com>
2024-08-05 20:02:45 +0300

    connected to mongo; started user schema

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,21 +1,37 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 import logo from "@public/assets/images/logo.svg";
-import profileImage from "@public/assets/images/logo.svg";
+// import profileImage from "@public/assets/images/logo.svg";
 
 const Nav = () => {
   const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   const isUserLoggedIn = true;
 
-  // const setProviders = async () => {};
+  const setAuthProviders = useCallback(async () => {
+    const response = await getProviders();
 
-  useEffect(() => {}, []);
+    setProviders(response);
+  }, []);
+
+  const handleToggleDropdown = () => {
+    setToggleDropdown((prev) => !prev);
+  };
+
+  const handleDropdownSignOut = () => {
+    handleToggleDropdown();
+    signOut();
+  };
+
+  useEffect(() => {
+    setAuthProviders();
+  }, []);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -53,7 +69,75 @@ const Nav = () => {
             </Link>
           </div>
         ) : (
-          <></>
+          <>
+            {providers &&
+              Object.values(providers).map((provider, index) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src={logo}
+              width={37}
+              height={37}
+              className="rounded-full cursor-pointer"
+              alt="profile"
+              onClick={handleToggleDropdown}
+            />
+
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={handleToggleDropdown}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={handleToggleDropdown}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  className="mt-5 w-full black_btn"
+                  onClick={handleDropdownSignOut}
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider, index) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
         )}
       </div>
     </nav>
