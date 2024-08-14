d81022da5a971f730061e2ece3e5e31dbbaf4594
Ionut Cristian Mihoci <ionutmihoci@yahoo.com>
2024-08-14 16:45:38 +0300

    feed page done

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((prompt) => (
+        <PromptCard
+          key={prompt._id}
+          prompt={prompt}
+          // handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const [prompts, setPrompts] = useState([]);
+
+  const handleChangeSearchText = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const fetchPosts = useCallback(async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setPrompts(data);
+  }, []);
+
+  const handleTagClick = useCallback((tag) => {
+    // Implement your logic to filter posts by tag here
+    // Example: setPosts(posts.filter((post) => post.tags.includes(tag)));
+  }, []);
+
+  useEffect(() => {
+    fetchPosts();
+  }, [fetchPosts]);
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for tags or a username"
+          value={searchText}
+          onChange={handleChangeSearchText}
+          required
+          className="search_input peer"
+        />
+        {/* <button type="submit">Search</button> */}
+      </form>
+
+      <PromptCardList data={prompts} handleTagClick={handleTagClick} />
+    </section>
+  );
 };
 
 export default Feed;
